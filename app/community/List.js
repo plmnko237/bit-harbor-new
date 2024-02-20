39b0a8ade641c1f9c5d2d0c80fe06d5103d7969e
plmnko <plmnko237@gmail.com>
2024-02-20 21:28:53 +0900

    게시판목록 제목->list클릭 시 상세페이지로 이동

--- app/community/List.js
+++ app/community/List.js
@@ -1,4 +1,4 @@
-import Title from "./Title";
+import Link from "next/link";
 
 export default function List({ dbData, i }) {
   const hideImages = (html) => {
@@ -14,37 +14,38 @@ export default function List({ dbData, i }) {
     return doc.body.innerHTML;
   };
   return (
-    <li className="conList" key={i}>
-      <div className="conList-top">
-        <Title dbData={dbData} i={i} />
-
-        <div className="card_detail">
-          <span className="category">{dbData.category}</span>
-          <div className="nickname">
-            <img
-              src={`/user_icon${dbData.profileNum}.png`}
-              alt="프로필이미지"
-            />
-            <span>{dbData.userNickname}</span>
-          </div>
-          <div className="viewsComment">
-            <div className="views">
-              <img src="/view.png" alt="조회수" />
-              <span>{dbData.view}</span>
+    <Link href={"/community/detail/" + dbData.communityId}>
+      <li className="conList" key={i}>
+        <div className="conList-top">
+          <p>{dbData.title}</p>
+          <div className="card_detail">
+            <span className="category">{dbData.category}</span>
+            <div className="nickname">
+              <img
+                src={`/user_icon${dbData.profileNum}.png`}
+                alt="프로필이미지"
+              />
+              <span>{dbData.userNickname}</span>
             </div>
-            <div className="comment">
-              <img src="/comment.png" alt="댓글" />
-              <span>{dbData.commentCount}</span>
+            <div className="viewsComment">
+              <div className="views">
+                <img src="/view.png" alt="조회수" />
+                <span>{dbData.view}</span>
+              </div>
+              <div className="comment">
+                <img src="/comment.png" alt="댓글" />
+                <span>{dbData.commentCount}</span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="conList-body">
-        <div
-          dangerouslySetInnerHTML={{ __html: hideImages(dbData.body) }}
-          className="listBody"
-        ></div>
-      </div>
-    </li>
+        <div className="conList-body">
+          <div
+            dangerouslySetInnerHTML={{ __html: hideImages(dbData.body) }}
+            className="listBody"
+          ></div>
+        </div>
+      </li>
+    </Link>
   );
 }
